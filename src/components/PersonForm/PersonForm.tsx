ab3ca735464251585ee2951d95bec7a33db9bc88
Sergey <serg.krutitsky2017@yandex.ru>
2023-06-25 15:25:55 +0300

    existing name check

--- src/components/PersonForm/PersonForm.tsx
+++ src/components/PersonForm/PersonForm.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 interface IPersonFormProps {
 	name: string;
+	error: string;
 	handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 	handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   }
 
-const PersonForm: React.FC<IPersonFormProps> = ({ name, handleChange, handleSubmit }) => {
+const PersonForm: React.FC<IPersonFormProps> = ({ name, error, handleChange, handleSubmit }) => {
 	return (
-	  <form onSubmit={handleSubmit}>
-		<input type="text" placeholder="Введите имя" value={name} onChange={handleChange} />
-		<button type="submit">Добавить</button>
-	  </form>
+		<>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="Введите имя" value={name} onChange={handleChange} />
+				<button type="submit">Добавить</button>
+			</form>
+			<h3 className='error'>{error}</h3>
+		</>
 	);
   }
   
